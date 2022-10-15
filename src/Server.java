419e3cdeb04a638af50445b93de1a54ae28fba2a
Zaborstik <zaborstik@gmail.com>
2022-10-15 23:44:41 +0300

    server moded

--- src/Server.java
+++ src/Server.java
@@ -4,20 +4,21 @@ import javax.sound.sampled.Port;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.NavigableMap;
 import java.util.concurrent.*;
 import java.util.Map;
 
 public class Server {
 
-    private static final Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
-    public static void sendBroadcastMessage(Message message){
+    public static void sendBroadcastMessage(Message message) {
         try {
             for (Connection connection :
                     connectionMap.values()) {
                 connection.send(message);
             }
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Не смогли отправить сообщение");
         }
     }
@@ -28,28 +29,69 @@ public class Server {
 
         int port = ConsoleHelper.readInt();
 
-        try (ServerSocket socket = new ServerSocket(port)){
+        try (ServerSocket socket = new ServerSocket(port)) {
             System.out.println("Сервер запущен");
 
-            while (true){
+            while (true) {
                 Thread thread = new Handler(socket.accept());
                 thread.start();
             }
-        } catch (Exception e){
-            System.out.println("Произошла ошибка");;
+        } catch (Exception e) {
+            System.out.println("Произошла ошибка");
+            ;
         }
     }
 
-    private static class Handler extends Thread{
+    private static class Handler extends Thread {
         private Socket socket;
 
-        public Handler (Socket socket){
+        public Handler(Socket socket) {
             this.socket = socket;
         }
 
         @Override
         public void run() {
 
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST, "Ввведите ваше имя"));
+
+                Message message = connection.receive();
+                if (message.getType()!=MessageType.USER_NAME){
+                    ConsoleHelper.writeMessage("Это не имя");
+                    continue;
+                }
+
+                String userName = message.getData();
+
+                if (userName.isEmpty()){
+                    ConsoleHelper.writeMessage("Попытка подключения без имени");
+                    continue;
+                }
+
+                if (connectionMap.containsKey(userName)){
+                    ConsoleHelper.writeMessage("Подключение по старому именю");
+                    continue;
+                }
+
+                connectionMap.put(userName, connection);
+
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+
+                return userName;
+            }
+        }
+
+        private void notifyUsers(Connection connection, String userName) throws IOException{
+            for (String name :
+                    connectionMap.keySet()) {
+                if (name.equals(userName)){
+                    continue;
+                }
+                connection.send(new Message(MessageType.USER_ADDED, name));
+            }
         }
     }
 }
